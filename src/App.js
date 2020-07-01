a8af5144c79b86a6d483062a353c689286f43ce7
nate buch <nate.buch@gmail.com>
2020-07-01 08:26:23 -0500

    Modified header and footer text

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [name] = useState("Nate") 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", flexDirection: "column", height: "100%"}}>
+      <div style={{ backgroundColor: "lightBlue"}}>
+        <div>
+            <Header/>
+        </div>
+      </div>
+      <div style={{ flex: 1 }}>
+        <Dashboard>
+          <DashboardBody/>
+          <DashboardName name={ name }/>
+        </Dashboard>
+           </div>
+      <div style={{ backgroundColor: "lightBlue"}}>
+        <div>
+          <Footer/>
+        </div>
+      </div>
     </div>
   );
+}
+
+function Header() {
+  return (
+    <div>
+      <h1>
+        Dashboard Header
+      </h1>
+    </div>
+  )
+}
+
+function Dashboard({ children }) {
+  return (
+    <div>
+      <h1>
+        Dashboard
+      </h1>
+      { children }
+    </div>
+  )
+}
+
+function DashboardBody() {
+  return (
+    <div>
+      <p>
+        This is the dashboard body and contains text.
+      </p>
+    </div>
+  )
+}
+
+function DashboardName({ name }) {
+  return (
+    <div>
+      <h3>
+        Sincerely, { name }
+      </h3>
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <div>
+      <h1>
+        Dashboard Footer
+      </h1>
+    </div>
+  )
 }
 
 export default App;
